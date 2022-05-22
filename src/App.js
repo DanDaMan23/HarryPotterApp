b3350111cbc8d93f59a0f10b413c62ba3c3b1b7c
DanDaMan23 <danlawrcons.gmail.com>
2022-05-22 00:56:23 -0500

    Routing Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, Routes } from "react-router-dom"
 import "./App.css"
 
 function App() {
   return (
     <div className='App'>
       <div className='box'>Harry Potter App</div>
+      <Routes>
+        <Route path='' element={<div>Home Page</div>} />
+        <Route path='Gryffindor' element={<div>Gryffindor</div>} />
+        <Route path='Hufflepuff' element={<div>Hufflepuff</div>} />
+        <Route path='Ravenclaw' element={<div>Ravenclaw</div>} />
+        <Route path='Slytherin' element={<div>Slytherin</div>} />
+      </Routes>
     </div>
   )
 }
