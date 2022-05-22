008bc4075dee63927e0bf247a3399bdca58f3f54
DanDaMan23 <danlawrcons.gmail.com>
2022-05-22 14:50:30 -0500

    Basic Axios Get House Data

--- src/features/houses/house.page.js
+++ src/features/houses/house.page.js
@@ -2,20 +2,28 @@ import React, { useEffect, useState } from "react"
 import PropTypes from "prop-types"
 import useAxios from "../../helpers/use-axios"
 import { HouseContext } from "./house.context"
+import Members from "./members/members"
 
 export default function HousePage({ houseName }) {
   const { get } = useAxios()
-  const getData = async () => {
-    const response = await get(
-      `http://hp-api.herokuapp.com/api/characters/house/${houseName.toLowerCase()}`
-    )
-    return response
-  }
+  const [houseState, setHouseState] = useState({ data: null, error: null })
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await get(
+        `http://hp-api.herokuapp.com/api/characters/house/${houseName.toLowerCase()}`
+      )
+      setHouseState(response)
+    }
+    fetchData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [houseName])
 
   return (
-    <HouseContext.Provider value={{ houseState: getData() }}>
+    <HouseContext.Provider value={{ houseState }}>
       <div className='container is-fluid'>
         <h1>{houseName}</h1>
+        <Members />
       </div>
     </HouseContext.Provider>
   )
