6bb75ea2ac0f09cd55360894cfcca1a2cc540aff
DanDaMan23 <danlawrcons.gmail.com>
2022-05-22 00:32:27 -0500

    Bulma Setup Completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css"
 
 function App() {
-  return <div className='App'>Harry Potter App</div>
+  return (
+    <div className='App'>
+      <div className='box'>Harry Potter App</div>
+    </div>
+  )
 }
 
 export default App
