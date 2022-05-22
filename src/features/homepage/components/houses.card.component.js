6164d97eb07d15be31b325f84ede0e4f6670068e
DanDaMan23 <danlawrcons.gmail.com>
2022-05-22 01:58:09 -0500

    Button Routes to the correct house

--- src/features/homepage/components/houses.card.component.js
+++ src/features/homepage/components/houses.card.component.js
@@ -1,12 +1,20 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { useNavigate } from "react-router-dom"
 
 import "./house.card.css"
 
 export default function HousesCard({ house }) {
+  const navigate = useNavigate()
+
   return (
     <div className='column is-half'>
-      <button className='button card-button'>
+      <button
+        className='button card-button'
+        onClick={() => {
+          navigate(house)
+        }}
+      >
         <figure className='image is-128x128'>
           <img
             src={require(`../../../assets/pictures/${house.toLowerCase()}.png`)}
