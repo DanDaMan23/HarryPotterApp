9b1fd67ef00b75f8c27d7016275aa835e2058789
DanDaMan23 <danlawrcons.gmail.com>
2022-05-23 14:24:02 -0500

    Logo Implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import HomepageRoutes from "./features/homepage/homepage.routes"
 
+import './App.css'
+
 export default function App() {
   return (
     <div className='App'>
-      <div className='box'>Harry Potter App</div>
+      <div className='box is-flex is-justify-content-center'>
+        <figure className='resize'>
+          <img
+            src={require("./assets/pictures/HarryPotterLogo.png")}
+            alt='Harry Potter Logo'
+          />
+        </figure>
+      </div>
       <HomepageRoutes />
     </div>
   )
